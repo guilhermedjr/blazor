c60407ea35beb6387e98b5197727d7759e29f6af
Guilherme Djrdjrjan Santos <guilhermedjrdjrjan@gmail.com>
2021-10-28 17:44:32 -0300

    Apply Code-Behind

---
+++ Blazor/BlazorWebAssemblyApp/Pages/IndexBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace BlazorWebAssemblyApp.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        protected class TodoItem
+        {
+            public string Title { get; set; }
+            public bool IsDone { get; set; }
+        }
+
+        protected string newTodo = "";
+        protected List<TodoItem> todos = new();
+
+        protected void AddNewTask()
+        {
+            if (!string.IsNullOrWhiteSpace(newTodo))
+            {
+                todos.Add(new TodoItem { Title = newTodo });
+                newTodo = "";
+            }
+        }
+    }
+}
